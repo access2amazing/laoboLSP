8f4f84007e65cc0267620b8b83cdf6d234375b82
xueli.wang <wang.xueli@amh-group.com>
2020-08-27 12:51:50 +0800

    first commit

---
+++ laobo/src/main/java/org/zhaoyangerqi/laobo/Application.java
@@ -0,0 +1,15 @@
+package org.zhaoyangerqi.laobo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xueli.wang
+ * @since 2020/08/27 12:49
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
